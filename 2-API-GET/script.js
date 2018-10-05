18464dad88ed01242b3451a9f093df07315bea7e
JulianaAmoasei <late.parrot@gmail.com>
2018-10-05 19:02:15 -0300

    alterando exercicio de API

--- 2-API-GET/script.js
+++ 2-API-GET/script.js
@@ -1,8 +1,8 @@
 const btnBusca = document.getElementById("btn-busca");
-btnBusca.addEventListener("click", trazBusca);
-let docs = [];
+btnBusca.addEventListener("click", trazResultadoDaBusca);
+let listaGifs = [];
 
-function buscaPalavra(){
+function buscaGif(){
   return document.getElementById("campo-busca").value;
 }
 
@@ -10,28 +10,26 @@ function erro(){
   console.log("erro");
 }
 
-function trazBusca(event){
+function trazResultadoDaBusca(event){
   event.preventDefault();
-  const pegarNoticia = new XMLHttpRequest();
-  pegarNoticia.open("GET", `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${buscaPalavra()}&api-key=132b29c8d05848999164b56f47037013`);
-  pegarNoticia.onload = carregaPosts;
-  pegarNoticia.onerror = erro;
-  pegarNoticia.send();
+  const respostaDaBusca = new XMLHttpRequest();
+  respostaDaBusca.open("GET", `http://api.giphy.com/v1/gifs/search?q=${buscaGif()}&api_key=COXu6QwBH0bSap1MazyjvIGVd5EFJDBS`);
+  respostaDaBusca.onload = carregaPostsComGifs;
+  respostaDaBusca.onerror = erro;
+  respostaDaBusca.send();
 }
 
-function carregaPosts(){
-  docs = JSON.parse(this.responseText)["response"]["docs"];
+function carregaPostsComGifs(){
+  listaGifs = JSON.parse(this.responseText)["data"];
   exibePosts();
 }
 
 function exibePosts(){
   let exibeBusca = document.getElementById("exibe-busca");
     exibeBusca.innerHTML = 
-      `<div class="area-noticia"> ${docs.map(doc => `
-        <div class="noticia">
-          <h3>${doc.headline.main}</h3>
-          <p>${doc.snippet}</p>
-          <a href="${doc.web_url}">Link para original</a>
+      `<div class="area-gif"> ${listaGifs.map(gif => `
+        <div class="gif">
+          <img src="${gif.images.fixed_height.url}"></img>
         </div>
         `).join("")}
       </div>`;
